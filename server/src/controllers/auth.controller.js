4b4479fe7bc747b4c759648903de66ef9ad8c0d2
Nayel Montiel <nayelmch@gmail.com>
2026-03-23 11:17:07 -0600

    endpoints de registro y login terminados, generan tokens y encriptan las contraseñas

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,53 +1,5 @@
 
 import { pool } from "../database/db.js";
-import { generarToken } from "../jwt.js"; 
-
-//metodo que valida un usuario en la base de datos 
-export const login = async (req, res) => {
-  try {
-    const { usuario, password } = req.body;
-    
-
-    //validar que los campos no vienen vacios 
-    if(!usuario || !password){
-      return res.status(400).json({
-        message : "usuario y contraseña requeridos"
-      })
-    }
-
-    //Buscar usuario en la BD
-    const [rows] = await pool.query(
-      "SELECT * FROM TBSGP_A_USUARIO WHERE TC_USU_NOMBRE = ?",
-      [usuario]
-    );
-
-    // Verifica si existe el usuario en la base de datos
-    if (rows.length === 0) {
-      return res.status(404).json({ message: "Usuario no existe" });
-    }
-
-        const user = rows[0];
-
-    //Validar contraseña (solo pruebas por el momento, ya que no se encriptan)
-    if (user.TC_USU_PASSWORD !== password) {
-      return res.status(401).json({
-
-        message: "Contraseña incorrecta",
-        
-        });
-    }
-        //login exitoso, regresa un token para el acceso de el usuario
-      
-    res.json({
-      message: "Login exitoso",
-      user: user.TC_USU_NOMBRE,
-
-      //llama la funcion que genera los token y los devuelve para que lo guarde el usuario
-      token: generarToken(user)
-    });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 
 import bcrypt from "bcryptjs";
 import { crearToken } from "../libs/jwt.js";
@@ -55,6 +7,63 @@ import {
   findByCorreoOIdentificacion,
   crearUsuario,
 } from "../models/usuario.model.js";
+
+//metodo que valida un usuario en la base de datos 
+
+
+export const login = async (req, res) => {
+  try {
+    const { correo, password } = req.body;
+
+    // Validar campos
+    if (!correo || !password) {
+      return res.status(400).json({
+        message: "Correo y contraseña requeridos"
+      });
+    }
+
+    // Buscar usuario por correo
+    const [rows] = await pool.query(
+      "SELECT * FROM TBSGP_A_USUARIO WHERE TC_USU_CORREO = ?",
+      [correo]
+    );
+
+    // Verificar si existe
+    if (rows.length === 0) {
+      return res.status(404).json({
+        message: "Usuario no existe"
+      });
+    }
+
+    const user = rows[0];
+
+    //Comparar contraseña encriptada
+    const passwordValida = await bcrypt.compare(
+      password,
+      user.TC_USU_PASSWORD
+    );
+
+    if (!passwordValida) {
+      return res.status(401).json({
+        message: "Contraseña incorrecta"
+      });
+    }
+
+    //Login exitoso
+    res.json({
+      message: "Login exitoso",
+      user: user.TC_USU_NOMBRE,
+      token: crearToken(user)
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+
+//registrar usuario en la db 
 
 export const register = async (req, res) => {
   const {
@@ -116,7 +125,3 @@ export const register = async (req, res) => {
   });
 };
 
-export const login = (req, res) => {
-  res.send("login");
-
-};
--- server/src/libs/jwt.js
+++ server/src/libs/jwt.js
@@ -8,3 +8,22 @@ export const crearToken = (payload) => {
 export const verificarToken = (token) => {
   return jwt.verify(token, JWT_SECRET);
 };
+
+
+
+//esta funcion se asegura que exista un token, se llama antes de entrar a una ruta siempre 
+export function ensureToken(req, res, next){
+    const bearerHeader = req.headers["authorization"];
+    console.log(bearerHeader)
+
+    if (typeof bearerHeader !== "undefined"){
+        const bearer = bearerHeader.split(" ")
+        const bearerToken = bearer[1]
+        req.token = bearerToken
+            next();
+    }
+    //en caso de que no haya token devuelve forbidden (no autorizado)
+    else{
+        res.sendStatus(403);
+    }
+}
